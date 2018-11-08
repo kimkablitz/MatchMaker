556127a9f17706740712f5ff97dd75c6c778849e
kimkablitz <kim.tran549@gmail.com>
2018-11-08 01:43:28 -0800

    did the math

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+        name: 'Kimmy Boo',
+        profilePic: 'https://pikdo.net/img/micha_kablitz.jpg?aHR0cHM6Ly9pbnN0YWdyYW0uZm11YzItMS5mbmEuZmJjZG4ubmV0L3ZwL2MxZDkxYjk5NjNiZDMwNjg3NDc3YmMxMDRlYTY3NjJjLzVDMTFCMDU1L3Q1MS4yODg1LTE5L3MxNTB4MTUwLzE4MzgwMjE4XzM2NjQ1NjM2Mzc1MTE5MF8xMTk2OTgzMTMxNzE0ODc5NDg4X2EuanBn',
+        scores: [
+          "4",
+          "5",
+          "5",
+          "2",
+          "4",
+          "5",
+          "4",
+          "4",
+          "3",
+          "4"
+        ]
+      }
+    
+];
+module.exports = friendList;
