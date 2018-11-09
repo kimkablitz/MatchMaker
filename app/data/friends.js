19fcdd5efcb90071038c4adf448f3e685e867c10
kimkablitz <kim.tran549@gmail.com>
2018-11-09 09:36:05 -0800

    added scott

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,14 @@
 var friendList = [
-    {
-        "name": 'Kimmy Boo',
-        "photo": 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/40428406_10216784791787522_6370845674442850304_n.jpg?_nc_cat=104&_nc_ht=scontent-sea1-1.xx&oh=71b02990d3d0836c42117414eca3927f&oe=5C6BBF4F',
-        "scores": [
-          "4",
-          "5",
-          "5",
-          "2",
-          "4",
-          "5",
-          "4",
-          "4",
-          "3",
-          "4"
-        ]
-      },
-      {
-        "name": "cibi",
-        "photo": "https://i.ytimg.com/vi/3ggIHfwkIWM/maxresdefault.jpg",
-        "scores": [
-        "5",
-        "3",
-        "4",
-        "1",
-        "3",
-        "5",
-        "4",
-        "2",
-        "5",
-        "1"
-        ]
-        }
-    
+  {
+    name: "Kimmy Boo",
+    photo:
+      "https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/40428406_10216784791787522_6370845674442850304_n.jpg?_nc_cat=104&_nc_ht=scontent-sea1-1.xx&oh=71b02990d3d0836c42117414eca3927f&oe=5C6BBF4F",
+    scores: ["4", "5", "5", "2", "4", "5", "4", "4", "3", "4"]
+  },
+  {
+    name: "Scott McMains",
+    photo: "https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/42981118_2216675325009739_7614750387563659264_n.jpg?_nc_cat=110&_nc_ht=scontent-sea1-1.xx&oh=5eea3eea2e0e9fa6d58095acb73224f0&oe=5C75600E",
+    scores: ["5", "3", "4", "1", "3", "5", "4", "2", "5", "1"]
+  }
 ];
 module.exports = friendList;
