d1b61bb5cb19b21d438a018dbed38a680d3041fb
kimkablitz <kim.tran549@gmail.com>
2018-11-09 18:16:06 -0800

    added 1 more object into friend.js

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MatchMaker
 
 
-The goal was to create a Friend Finder app (essentially a dating app but for friends) using Node and Express servers. The user enters the page that leads to a survey. When the user submits the survey, they are matched with a "friend" that has the closest match to their personality.
+The goal was to create a Match Maker app (essentially a dating app but for friends) using Node and Express servers. The user enters the page that leads to a survey. When the user submits the survey, they are matched with a "friend" that has the closest match to their personality.
 
 # Challenges Encountered
 
--- app/data/friends.js
+++ app/data/friends.js
@@ -9,6 +9,11 @@ var friendList = [
     name: "Scott McMains",
     photo: "https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/42981118_2216675325009739_7614750387563659264_n.jpg?_nc_cat=110&_nc_ht=scontent-sea1-1.xx&oh=5eea3eea2e0e9fa6d58095acb73224f0&oe=5C75600E",
     scores: ["5", "3", "4", "1", "3", "5", "4", "2", "5", "1"]
+  },
+  {
+    name: "Kim Kablitz",
+    photo: "https://scontent-sea1-1.xx.fbcdn.net/v/t31.0-8/19055194_10212547760784395_123070911908255333_o.jpg?_nc_cat=100&_nc_ht=scontent-sea1-1.xx&oh=a2471fb2d5b4f67aecd508889f161892&oe=5C801DBE",
+    scores: ["5", "5", "3", "1", "4", "1", "1", "5", "1", "2"]
   }
 ];
 module.exports = friendList;
